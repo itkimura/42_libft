faee03178345cebe80efcae3762a30a509235f46
ikimura <ikimura@student.42tokyo.jp>
2021-11-27 23:51:40 +0200

    Fix norminette error

--- ft_lstadd.c
+++ ft_lstadd.c
@@ -6,13 +6,13 @@
 /*   By: itkimura <itkimura@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 11:48:48 by itkimura          #+#    #+#             */
-/*   Updated: 2021/11/24 12:00:10 by itkimura         ###   ########.fr       */
+/*   Updated: 2021/11/26 21:54:48 by itkimura         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_lstadd(t_list **alst, t_list *new)
+void	ft_lstadd(t_list **alst, t_list *new)
 {
 	if (alst && new)
 	{
