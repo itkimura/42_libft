31e0510304cfe759cf7804bb976eddb0f7aa41ad
Itoe Kimura <itkimura@c1r1p6.hive.fi>
2021-12-07 13:34:57 +0200

    Update atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,42 +6,31 @@
 /*   By: itkimura <itkimura@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 15:27:13 by itkimura          #+#    #+#             */
-/*   Updated: 2021/11/29 16:16:27 by itkimura         ###   ########.fr       */
+/*   Updated: 2021/12/01 12:08:46 by itkimura         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static const char	*remove_space(const char *str, int *flag)
-{
-	*flag = 1;
-	while (ft_isspace(*str))
-		str++;
-	if (*str == '-' || *str == '+')
-	{
-		if (*str == '-')
-			*flag = *flag * -1;
-		str++;
-	}
-	return (str);
-}
 
 int	ft_atoi(const char *str)
 {
 	int	flag;
 	int	dest;
 
+	flag = 1;
 	dest = 0;
-	str = remove_space(str, &flag);
-	if (*str >= '0' && *str <= '9')
+	while (ft_isspace(*str))
+		str++;
+	if (*str == '-' || *str == '+')
 	{
-		while (*str >= '0' && *str <= '9')
-		{
-			dest = (dest * 10) + (*str - '0');
-			str++;
-		}
-		return (dest * flag);
+		if (*str == '-')
+			flag = -1;
+		str++;
 	}
-	else
-		return (0);
+	while (*str >= '0' && *str <= '9' && *str)
+	{
+		dest = (dest * 10) + (*str - '0');
+		str++;
+	}
+	return (dest * flag);
 }
--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: itkimura <itkimura@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/15 11:20:44 by itkimura          #+#    #+#             */
-/*   Updated: 2021/11/15 11:50:03 by itkimura         ###   ########.fr       */
+/*   Updated: 2021/12/01 12:21:19 by itkimura         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,11 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 
 	i = 0;
 	if (dst == src)
-		return ((unsigned char *)dst);
+		return (dst);
 	while (i < n)
 	{
 		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
 		i++;
 	}
-	return ((unsigned char *)dst);
+	return (dst);
 }
